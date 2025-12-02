2bc1e73de01944e69fcd79ec4e14e157f6c3ceb4
TheGreatMegalodon <atomes.600@gmail.com>
2025-12-02 19:37:19 +0100

    Update image source for improved visual quality

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -48,7 +48,7 @@ export default function Home({ isPageActive, goTo }) {
 
                 <div className="present-in-macbook">
                     <img className="device" src="https://raw.githubusercontent.com/Gabriel-Jagueneau/weatherPod/refs/heads/main/storage/images/macbook.png" alt="macbook-image"/>
-                    <img className="image" src="https://raw.githubusercontent.com/Gabriel-Jagueneau/weatherPod/refs/heads/main/storage/images/background/28efb1394876b5ab7708a986a93292d5.jpeg" alt="" />
+                    <img className="image" src="https://raw.githubusercontent.com/Gabriel-Jagueneau/weatherPod/refs/heads/main/storage/images/background/28efb1394876b5ab7708a986a93292d5_upscayl_4x_upscayl-standard-4x.png" alt="" />
                 </div>
                 
             </section>
